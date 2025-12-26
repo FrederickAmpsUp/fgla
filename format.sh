935298a444c991df4296661e60a977e7b065a233
FrederickAmpsUp <frederick.ziola@gmail.com>
2025-12-26 11:58:49 -0500

    Update formatting utilities

--- format.sh
+++ format.sh
@@ -1,1 +1,3 @@
-find . -name '*.hpp' -o -name '*.cpp' -o -name '*.c' -o -name '*.h' | grep -v '^./extern/' | xargs clang-format -i
+find . -name '*.hpp' -o -name '*.cpp' -o -name '*.c' -o -name '*.h' \
+  | grep -v '^./extern/' \
+  | xargs -P "$(nproc)" -I{} sh -c 'echo "Formatting {}"; clang-format -i "{}"'
