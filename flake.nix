f54d7e20b9fb4122ddc2d1d538972e59089e3b98
Frederick Ziola <frederick.ziola@gmail.com>
2026-08-04 23:31:19 -0400

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "fgla development environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          gcc
+          clang
+          cmake
+          ninja
+
+          glfw
+          vulkan-loader
+          vulkan-headers
+          vulkan-tools
+          vulkan-validation-layers
+        ];
+      };
+    };
+}
