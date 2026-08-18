f4920a18e926923c273b1ceb19ca7883ab8c2778
FrederickAmpsUp <frederick.ziola@gmail.com>
2026-08-17 20:34:16 -0400

    Update fgla.hpp

--- include/fgla/fgla.hpp
+++ include/fgla/fgla.hpp
@@ -1,11 +1,26 @@
 #pragma once
 
+// Core types / utilities
+#include <fgla/color.hpp>
+#include <fgla/error.hpp>
+#include <fgla/types.hpp>
+#include <fgla/util.hpp>
+
+// Core objects
 #include <fgla/adapter.hpp>
 #include <fgla/backend.hpp>
+#include <fgla/completion.hpp>
 #include <fgla/device.hpp>
-#include <fgla/error.hpp>
 #include <fgla/extension.hpp>
 #include <fgla/instance.hpp>
 #include <fgla/queue.hpp>
-#include <fgla/types.hpp>
-#include <fgla/util.hpp>
+
+// Resources
+#include <fgla/image.hpp>
+#include <fgla/image_view.hpp>
+#include <fgla/shader_module.hpp>
+
+// Commands / rendering
+#include <fgla/command_buffer.hpp>
+#include <fgla/render_pass.hpp>
+#include <fgla/render_pipeline.hpp>
