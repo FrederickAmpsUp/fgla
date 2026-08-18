d277cf1581a89afdaf2ba158f7b58d321d199c2e
FrederickAmpsUp <frederick.ziola@gmail.com>
2026-08-17 22:28:49 -0400

    Refactor to objgen interface

--- include/fgla/command_buffer.hpp
+++ include/fgla/command_buffer.hpp
@@ -1,41 +1,31 @@
 #pragma once
 
 #include <fgla/error.hpp>
-#include <fgla/internal.hpp>
+#include <fgla/object_gen.hpp>
 #include <fgla/render_pass.hpp>
-#include <memory>
 
 namespace fgla {
 
-/// Represents a command buffer, which is used to record commands before
-/// submitting to the GPU
-class CommandBuffer {
-public:
-  inline Result<RenderPass>
-  begin_render_pass(const RenderPass::Descriptor &desc) {
-    return this->impl->begin_render_pass(desc);
-  }
+#define FGLA_OBJ_NAME CommandBuffer
+/**
+ * Represents a command buffer, which is used to record commands before
+ * submitting to the GPU
+ */
+FGLA_OBJ_START
 
-  /// The backend-defined implementation of the `CommandBuffer`'s functions
-  struct Impl {
-    virtual Result<RenderPass>
-    begin_render_pass(const RenderPass::Descriptor &desc) = 0;
+#define FGLA_OBJ_FUNCTIONS(FN)                                                 \
+  FN(/**                                                                       \
+      * Begins a `RenderPass` using the specified descriptor                   \
+      * @param desc The `RenderPass::Descriptor` describing the render pass    \
+      * @returns The created `RenderPass`, or an `Error` with failure          \
+      * information                                                            \
+      */                                                                       \
+     , Result<RenderPass>, begin_render_pass,                                  \
+     (const RenderPass::Descriptor &desc), (desc))
 
-    virtual ~Impl() = 0;
-  };
+FGLA_OBJ_END
 
-  /// Creates a `CommandBuffer` from a raw implementation
-  /// This should only be used internally
-  static inline CommandBuffer from_raw(std::unique_ptr<Impl> impl) {
-    CommandBuffer command_buffer;
-    command_buffer.impl = std::move(impl);
-    return command_buffer;
-  }
+#undef FGLA_OBJ_NAME
+#undef FGLA_OBJ_FUNCTIONS
 
-private:
-  friend struct fgla::internal::ImplAccessor;
-  std::unique_ptr<Impl> impl;
-};
-
-inline CommandBuffer::Impl::~Impl() = default;
 } // namespace fgla
