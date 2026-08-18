caa633f25abef70ee556cca5b8f36a9493074a01
FrederickAmpsUp <frederick.ziola@gmail.com>
2026-08-17 22:46:43 -0400

    Refactor to objectgen interface

--- include/fgla/device.hpp
+++ include/fgla/device.hpp
@@ -1,77 +1,62 @@
 #pragma once
 
 #include <fgla/error.hpp>
-#include <fgla/internal.hpp>
+#include <fgla/object_gen.hpp>
 #include <fgla/queue.hpp>
 #include <fgla/render_pipeline.hpp>
 #include <fgla/shader_module.hpp>
 #include <fgla/util.hpp>
 #include <filesystem>
-#include <memory>
 #include <optional>
 #include <vector>
 
 namespace fgla {
 
-/// Represents a device, which is used to create rendering objects
-class Device {
+#define FGLA_OBJ_NAME Device
+/**
+ * Represents a device, which is used to create rendering objects
+ */
+FGLA_OBJ_START
+/// Represents the settings used to create a `Device`
+struct Descriptor {
 public:
-  /// Represents the settings used to create a `Device`
-  struct Descriptor {
-  public:
-    /// A number of directories in which to search for shader source modules
-    const std::vector<std::filesystem::path> &shader_paths;
-  };
-
-  /// Retrieve a `Queue` of the given type and index
-  ///
-  /// @param type The type of queue to retrieve (e.g., `Queue::Type::Graphics`
-  /// or `Queue::Type::Transfer`)
-  /// @param index The index of the queue in the specified type
-  /// @returns The specified queue, or `std::nullopt` if not found
-  inline std::optional<std::reference_wrapper<Queue>>
-  get_queue(Queue::Type type, uint32_t index) {
-    Queue *q = this->impl->get_queue(type, index);
-    if (q)
-      return *q;
-    else
-      return {};
-  }
-
-  inline Result<ShaderModule>
-  load_shader_module(const ShaderModule::Descriptor &desc) {
-    return this->impl->load_shader_module(desc);
-  }
-
-  inline Result<RenderPipeline>
-  create_render_pipeline(const RenderPipeline::Descriptor &desc) {
-    return this->impl->create_render_pipeline(desc);
-  }
-
-  /// The backend-defined implementation of the `Device`'s functions
-  struct Impl {
-    virtual Queue *get_queue(Queue::Type, uint32_t) = 0;
-
-    virtual Result<ShaderModule>
-    load_shader_module(const ShaderModule::Descriptor &) = 0;
-    virtual Result<RenderPipeline>
-    create_render_pipeline(const RenderPipeline::Descriptor &) = 0;
-
-    virtual ~Impl() = 0;
-  };
-
-  /// Creates a `Device` from a raw implementation
-  /// This should only be used internally
-  static inline Device from_raw(std::unique_ptr<Impl> impl) {
-    Device device;
-    device.impl = std::move(impl);
-    return device;
-  }
-
-private:
-  friend struct fgla::internal::ImplAccessor;
-  std::unique_ptr<Impl> impl;
+  /// A number of directories in which to search for shader source modules
+  const std::vector<std::filesystem::path> &shader_paths;
 };
+#define FGLA_OBJ_FUNCTIONS(FN)                                                 \
+  FN(/**                                                                       \
+      * Retrieves a `Queue` of the given type and index                        \
+      *                                                                        \
+      * @param type The type of queue to retrieve                              \
+      * @param index The index of the queue in the specified type              \
+      * @returns The specified queue, or `std::nullopt` if not found           \
+      */                                                                       \
+     , std::optional<std::reference_wrapper<Queue>>, get_queue,                \
+     (Queue::Type type, uint32_t index), (type, index))                        \
+  FN(/**                                                                       \
+      * Loads a shader module from a file                                      \
+      *                                                                        \
+      * @param desc The `ShaderModule::Descriptor` describing which module to  \
+      * load                                                                   \
+      * @returns The loaded `ShaderModule`, or an `Error` with failure         \
+      * information                                                            \
+      */                                                                       \
+     , Result<ShaderModule>, load_shader_module,                               \
+     (const ShaderModule::Descriptor &desc), (desc))                           \
+  FN(/**                                                                       \
+      * Creates a `RenderPipeline`                                             \
+      *                                                                        \
+      * @param desc The `RenderPipeline::Descriptor` containing settings for   \
+      * the render pipeline to be created                                      \
+      * @returns The created `RenderPipeline`, or an `Error` with failure      \
+      * information                                                            \
+      */                                                                       \
+     , Result<RenderPipeline>, create_render_pipeline,                         \
+     (const RenderPipeline::Descriptor &desc), (desc))
 
-inline Device::Impl::~Impl() = default;
+FGLA_OBJ_END
+
+#undef FGLA_OBJ_NAME
+#undef FGLA_OBJ_FUNCTIONS
+
 } // namespace fgla
